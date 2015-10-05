171203f0a150315aed3d9fbdcb5bef912c195df6
Tanuj <tanujnay112@live.com>
2015-10-04 19:05:01 -0700

    Updated activity_main to a LinearLayout scheme.

--- app/src/main/java/interlakeprogrammingclub/bsdgradechecker/Login.java
+++ app/src/main/java/interlakeprogrammingclub/bsdgradechecker/Login.java
@@ -1,31 +1,17 @@
 package interlakeprogrammingclub.bsdgradechecker;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class Login extends AppCompatActivity {
+public class Login extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
