9c5e6b61c741b494fb7e30a2ff60629ff01f5e18
Peachball <chen_xu@live.com>
2015-10-09 19:27:00 -0700

    Added new activity for actually viewing grades

---
+++ app/src/main/java/interlakeprogrammingclub/bsdgradechecker/GradesViewerActivity.java
@@ -0,0 +1,20 @@
+package interlakeprogrammingclub.bsdgradechecker;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class GradesViewerActivity extends Activity {
+    //too lzay to do anythig here
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_grades_viewer);
+
+    }
+
+}
