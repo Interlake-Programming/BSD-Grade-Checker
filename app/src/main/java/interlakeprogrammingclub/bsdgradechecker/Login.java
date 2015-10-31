cb51ec344dc3d7007754e210fbf2371776855628
Peachball <chen_xu@live.com>
2015-10-31 12:17:05 -0700

    Jsoup doesn't appear to work, and neither does the old set headers thing

--- app/src/main/java/interlakeprogrammingclub/bsdgradechecker/Login.java
+++ app/src/main/java/interlakeprogrammingclub/bsdgradechecker/Login.java
@@ -20,7 +20,13 @@ import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
 
 
 public class Login extends Activity implements View.OnClickListener {
@@ -102,20 +108,68 @@ public class Login extends Activity implements View.OnClickListener {
         @Override
         protected void onPreExecute(){
             spinner.setVisibility(View.VISIBLE);
-            screen.setVisibility(View.GONE);
         }
+
         @Override
         protected String doInBackground(String... params) {
             String html;
+            html = getHTML();
             try {
-                Connection.Response res = Jsoup.connect("https://aspen.bsd405.org/aspen/logon.do")
-                        .data("username", "s-xuch", "password", "pewdiepieduck")
+                Document a = Jsoup.parse("https://aspen.bsd405.org/");
+                Connection con = Jsoup.connect("https://aspen.bsd405.org/aspen/logon.do").data("username","s-xuch","password","pewdiepieduck")
                         .method(Connection.Method.POST)
-                        .execute();
+                        .cookie("JSESSIONID","2BA68E361ED63F15B5F8A16EAF8E5924");
+                Connection.Response res = con.execute();
                 Document doc = res.parse();
             }
             catch(IOException e){
                 Log.d("Exception", "couldn't connect?");
+            }
+            return null;
+        }
+
+        private String getHTML(){
+            try {
+                String output = "org.apache.struts.taglib.html.TOKEN=8c2cc97dcf02931f630ed3f77486e26b&userEvent=930&userParam=&operationId=&deploymentId=x2sis&scrollX=0&scrollY=0&formFocusField=username&mobile=false&SSOLoginDone=&username=" + "s-xuch" + "&password=" + "pewdiepieduck";
+
+                //Set up the connection
+                URL url = new URL("https://aspen.bsd405.org/aspen/logon.do");
+                HttpsURLConnection c = (HttpsURLConnection) url.openConnection();
+                c.setDoInput(true);
+                c.setDoOutput(true);
+                c.setRequestMethod("POST");
+                c.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.99 Safari/537.36");
+                c.setRequestProperty("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8");
+                c.setRequestProperty("Content-Length", Integer.toString(output.length()));
+                c.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+                c.setRequestProperty("Accept-Encoding", "gzip, deflate");
+                c.setRequestProperty("Accept-Language", "en-US,en;q=0.8");
+                c.setRequestProperty("Cache-Control", "max-age=0");
+                c.setRequestProperty("Connection", "keep-alive");
+                c.setRequestProperty("Cookie", "JSESSIONID=C2793475E98D6FDBAA9CBCF3583DA371; deploymentId=x2sis; __utma=261430046.705141245.1434430510.1441164336.1441239179.12; __utmz=261430046.1435202913.5.3.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=(not%20provided); _ga=GA1.3.705141245.1434430510");
+                c.setRequestProperty("Host", "aspen.bsd405.org");
+                c.setRequestProperty("Origin", "https://aspen.bsd405.org");
+                c.setRequestProperty("Referer", "https://aspen.bsd405.org/aspen/logon.do");
+
+                //Write the form data out
+                OutputStream out = c.getOutputStream();
+                out.write(output.getBytes("UTF-8"));
+                out.flush();
+                out.close();
+                c.connect();
+                System.out.println(c.getResponseCode());
+
+                BufferedReader in = new BufferedReader(new InputStreamReader(c.getInputStream()));
+                String iline = in.readLine();
+                StringBuffer o = new StringBuffer();
+                while (iline != null) {
+                    o.append(iline + '\n');
+                    iline = in.readLine();
+                }
+                return o.toString();
+            }
+            catch(Exception e){
+                Log.d("error",e.toString());
             }
             return null;
         }
@@ -126,4 +180,5 @@ public class Login extends Activity implements View.OnClickListener {
             startActivity(new Intent(Login.this, GradesViewerActivity.class));
         }
     }
+
 }
